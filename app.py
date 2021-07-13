a7e8b870d489e8ba457e29a75d00918ef40ea981
Ashton Lobo <ashton.lobo@stud.hochschule-heidelberg.de>
2021-07-13 18:35:28 +0200

    Modified csv path

--- app.py
+++ app.py
@@ -102,7 +102,9 @@ def predict():
 
     train_X, test_X, train_y, test_y = train_test_split(features, target, test_size = 0.2, shuffle=False, stratify=None)
     
-    model = load_model('P:/BDBA/SEM_4/Analytics_4/Stock_prediction/LSTM_E_D_model_1.h5')
+    # model = load_model('P:/BDBA/SEM_4/Analytics_4/Stock_prediction/LSTM_E_D_model_1.h5')
+    model = load_model('https://raw.githubusercontent.com/ashton77/Stock-Price-Prediction-LSTM-/master/LSTM_E_D_model_1.h5')
+
 
     data_df_date = list(data_df.index.values)
 
